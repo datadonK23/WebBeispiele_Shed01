dfc14565acd0df335901759f37a090c3936821c1
donk23 <datadonk23@gmail.com>
2015-01-28 16:11:16 +0100

    setting up flask template for app

---
+++ TourismAT/app/views.py
@@ -0,0 +1,10 @@
+"""
+Views
+"""
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
---
+++ TourismAT/instance/config.py
@@ -0,0 +1,7 @@
+"""
+Secret Configurations
+API KEY's, DB Settings etc
+"""
+
+# Override debugging settings for development
+DEBUG = True
