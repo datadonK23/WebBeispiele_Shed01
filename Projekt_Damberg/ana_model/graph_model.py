1f43d9ea4bcf24edba54362c53a8d2aa7110d29c
donk23 <datadonk23@gmail.com>
2015-02-08 20:41:38 +0100

    working on graph model and cleaning geo data

--- Projekt_Damberg/ana_model/graph_model.py
+++ Projekt_Damberg/ana_model/graph_model.py
@@ -7,46 +7,79 @@ Date: 08.02.2015
 """
 
 import networkx as net
+import matplotlib.pyplot as plt
 
-# Einfaches Modell 5 Auffahrten
-simpleG = net.Graph()
+# Einfaches Modell 6 Auffahrten
+simpleG6 = net.DiGraph()
+simpleG5 = net.DiGraph()
 
 # node A
-simpleG.add_edge("A", "B")
-simpleG.add_edge("A", "C")
-simpleG.add_edge("A", "D")
-simpleG.add_edge("A", "E")
+simpleG6.add_edge("A", "D")
+simpleG6.add_edge("A", "E")
+simpleG6.add_edge("A", "F")
 
+simpleG5.add_edge("A", "D")
+simpleG5.add_edge("A", "E")
 
 # node B
-simpleG.add_edge("B", "A")
-simpleG.add_edge("B", "C")
-simpleG.add_edge("B", "D")
-simpleG.add_edge("B", "E")
+simpleG6.add_edge("B", "D")
+simpleG6.add_edge("B", "E")
+simpleG6.add_edge("B", "F")
+
+simpleG5.add_edge("B", "D")
+simpleG5.add_edge("B", "E")
 
 # node C
-simpleG.add_edge("C", "A")
-simpleG.add_edge("C", "B")
-simpleG.add_edge("C", "D")
-simpleG.add_edge("C", "E")
+simpleG6.add_edge("C", "D")
+simpleG6.add_edge("C", "E")
+simpleG6.add_edge("C", "F")
+
+simpleG5.add_edge("C", "D")
+simpleG5.add_edge("C", "E")
 
 # node D
-simpleG.add_edge("D", "A")
-simpleG.add_edge("D", "B")
-simpleG.add_edge("D", "C")
-simpleG.add_edge("D", "E")
+simpleG6.add_edge("D", "A")
+simpleG6.add_edge("D", "B")
+simpleG6.add_edge("D", "C")
+
+simpleG5.add_edge("D", "A")
+simpleG5.add_edge("D", "B")
+simpleG5.add_edge("D", "C")
 
 # node E
-simpleG.add_edge("E", "A")
-simpleG.add_edge("E", "B")
-simpleG.add_edge("E", "C")
-simpleG.add_edge("E", "D")
+simpleG6.add_edge("E", "A")
+simpleG6.add_edge("E", "B")
+simpleG6.add_edge("E", "C")
+
+simpleG5.add_edge("E", "A")
+simpleG5.add_edge("E", "B")
+simpleG5.add_edge("E", "C")
+
+# node F
+simpleG6.add_edge("F", "A")
+simpleG6.add_edge("F", "B")
+simpleG6.add_edge("F", "C")
 
 
 # Drawing
-net.draw(simpleG)
+
+plot1 = plt.figure()
+ax1 = plot1.add_subplot(111)
+net.draw(simpleG6, ax=ax1)
+
+plot2 = plt.figure()
+ax2 = plot2.add_subplot(111)
+net.draw(simpleG5, ax=ax2)
 
 # Analyse
-if net.is_eulerian(simpleG):
-    print list(net.eulerian_circuit(simpleG, source="B"))
+if net.is_eulerian(simpleG6):
+    print "6 Auffahrten: " + str(list(net.eulerian_circuit(simpleG6,
+                                                           source="B")))
+else:
+    print "6: Kein Eulerweg"
 
+if net.is_eulerian(simpleG5):
+    print "5 Auffahrten: " + str(list(net.eulerian_circuit(simpleG5,
+                                                           source="B")))
+else:
+    print "5: Kein Eulerweg"
