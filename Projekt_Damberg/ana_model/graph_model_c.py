88d4567751c6054993dc79454890b1ecaa8005f0
donk23 <datadonk23@gmail.com>
2015-02-13 22:03:11 +0100

    model with culmination point and start model without

--- Projekt_Damberg/ana_model/graph_model_c.py
+++ Projekt_Damberg/ana_model/graph_model_c.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Graph Model with Culmination Point
+Graph Model without Culmination Point
 
 @author: Thomas Treml (datadonk23@gmail.com)
 Date: 13.02.2015
@@ -13,58 +13,47 @@ import matplotlib.pyplot as plt
 G = net.DiGraph()
 
 # node A
-G.add_edge("A", "X", weight=5)
+G.add_edge("A", "G", weight=5)
 G.add_edge("A", "B", weight=100)
 
 # node B
-G.add_edge("B", "X", weight=9)
+G.add_edge("B", "H", weight=9)
 G.add_edge("B", "A", weight=100)
 G.add_edge("B", "C", weight=100)
 
 # node C
-G.add_edge("C", "X", weight=1)
+G.add_edge("C", "F", weight=1)
 G.add_edge("C", "B", weight=100)
 G.add_edge("C", "D", weight=100)
 
 # node D
-G.add_edge("D", "X", weight=8)
+G.add_edge("D", "E", weight=8)
 G.add_edge("D", "C", weight=100)
 G.add_edge("D", "I", weight=100)
 
 # node E
-G.add_edge("E", "X", weight=2)
+G.add_edge("E", "D", weight=2)
 G.add_edge("E", "F", weight=100)
 
 # node F
-G.add_edge("F", "X", weight=7)
+G.add_edge("F", "S", weight=7)
 G.add_edge("F", "E", weight=100)
 G.add_edge("F", "G", weight=100)
 
 # node G
-G.add_edge("G", "X", weight=4)
+G.add_edge("G", "A", weight=4)
 G.add_edge("G", "F", weight=100)
 G.add_edge("G", "H", weight=100)
 
 # node H
-G.add_edge("H", "X", weight=3)
+G.add_edge("H", "C", weight=3)
 G.add_edge("H", "G", weight=100)
 G.add_edge("H", "I", weight=100)
 
 # node I
-G.add_edge("I", "X", weight=6)
+G.add_edge("I", "B", weight=6)
 G.add_edge("I", "H", weight=100)
 G.add_edge("I", "D", weight=100)
-
-# node X
-G.add_edge("X", "A", weight=10)
-G.add_edge("X", "B", weight=10)
-G.add_edge("X", "C", weight=10)
-G.add_edge("X", "D", weight=10)
-G.add_edge("X", "E", weight=10)
-G.add_edge("X", "F", weight=10)
-G.add_edge("X", "G", weight=10)
-G.add_edge("X", "H", weight=10)
-G.add_edge("X", "I", weight=10)
 
 
 # Drawing
@@ -77,15 +66,23 @@ net.draw_networkx_labels(G, pos=pos)
 
 
 # Routing Algorithm
-def best_route(graph=G, culm="X"):
+def best_route(graph=G):
     """Finds best combination (from most to least exhausting) of cyling 
        ascendends in given weighted graph
-    In: graph, culmination point
+    In: graph
     Out: list of path tuples past by best track
     """
     route = []
     
-    while (len(graph) > 1):
+    nextN = sorted(graph.edges(data=True), key= lambda edge: edge[2])
+    start = nextN[0][0]
+    target = nextN[0][1]
+    path = net.dijkstra_path(graph, start, target)
+    route.append(path)        
+    graph.remove_edge(path[0], path[1])
+# FIXME find next node
+    
+    """while (len(graph) > 1):
         nextN = sorted(graph.edges(data=True), key= lambda edge: edge[2])[0][0]
         asc = net.dijkstra_path(graph, nextN, culm)
         graph.remove_node(asc[0])
@@ -104,8 +101,7 @@ def best_route(graph=G, culm="X"):
     
     asc_top = ("X", "S")    
     route.append(asc_top)
-    
+    """
     return route
     
 route = best_route()
-
