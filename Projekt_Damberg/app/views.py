84819cb0d2e62b21163be51677aca01d50e04254
donk23 <datadonk23@gmail.com>
2015-02-18 16:11:31 +0100

    app navigation

--- Projekt_Damberg/app/views.py
+++ Projekt_Damberg/app/views.py
@@ -8,3 +8,20 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/challenge')
+def challenge():
+    return render_template('challenge.html')
+
+@app.route('/tracks')
+def tracks():
+    return render_template('tracks.html')
+  
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/routing')
+def routing():
+    return render_template('routing.html')
+
