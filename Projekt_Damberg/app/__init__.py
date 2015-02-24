73136648171c80259386a1380a1118ecfc5864d6
donk23 <datadonk23@gmail.com>
2015-02-24 19:18:20 +0100

    db model template

--- Projekt_Damberg/app/__init__.py
+++ Projekt_Damberg/app/__init__.py
@@ -3,10 +3,14 @@ Initialise App
 """
 
 from flask import Flask
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
 app.config.from_pyfile("config.py")
 
+#mongo = PyMongo(app)
+
 from app import views
+from app import controller
 from app import models
