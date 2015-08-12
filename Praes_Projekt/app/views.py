abcd668921dc8a6250a9b1ec9a302b030e223ff9
donk23 <datadonk23@gmail.com>
2015-08-12 18:18:25 +0200

    trying MDL layout

--- Praes_Projekt/app/views.py
+++ Praes_Projekt/app/views.py
@@ -5,6 +5,29 @@ Views
 from flask import render_template
 from app import app
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route("/survey")
+def survey():
+    return render_template("survey.html")
+
+
+#FIXME
+@app.route("/recommend")
+def recommend():
+    return render_template("index.html")
+
+#FIXME
+@app.route("/result")
+def result():
+    return render_template("index.html")
+
+#FIXME
+@app.route("/ref")
+def ref():
+    return render_template("ref.html")
+
+
+
